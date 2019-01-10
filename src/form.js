b9ba13668caf870081d27556dd8ed052159d4542
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2019-01-10 17:19:28 +0100

    form is rendering fine

---
+++ src/form.js
@@ -0,0 +1,92 @@
+  
+
+  function formLoader(parentNode) {
+    let form = document.createElement('form');
+
+
+    // HTML Tag Generator Functions
+    const labelMaker = (title, purpose) => {
+     let label = document.createElement('label');
+     label.purpose = purpose;
+     label.innerHTML = title;
+     return label;
+    }
+
+    const inputMaker = (type, classList, id, placeholder) => {
+      let input = document.createElement('input');
+      input.placeholder = placeholder;
+      input.type = type;
+      input.classList = classList;
+      input.id = id;
+      return input;
+     }
+
+     const classDivMaker = (className) => {
+       let div = document.createElement('div');
+       div.className = className;
+       return div;
+     }
+
+     const mySelectMaker = (className, options) => {
+       let select = document.createElement('select');
+       select.className = className;
+
+       for (let option of options) { 
+         let tag = document.createElement('option');
+         tag.innerHTML = option;
+         select.appendChild(tag);
+       }
+       return select;
+     }
+
+    //  End of HTML Tag Generator Functions
+
+
+    //  Start of the Title Div creation
+
+    const titleLabel = labelMaker("Title", "toDoTitle");
+    const titleInput = inputMaker('text', 'form-control', 'title-input', 'Enter Title');
+    const titleDiv = classDivMaker('form-group');
+    titleDiv.appendChild(titleLabel);
+    titleDiv.appendChild(titleInput)
+    form.appendChild(titleDiv);
+
+    // End of the Title Div creation
+
+
+    //  Start of the Description Div creation
+
+    const descriptionLabel = labelMaker("Description", "toDodescription");
+    const descriptionInput = inputMaker('text', 'form-control', 'description-input', 'Enter Description');
+    const descriptionDiv = classDivMaker('form-group');
+    descriptionDiv.appendChild(descriptionLabel);
+    descriptionDiv.appendChild(descriptionInput)
+    form.appendChild(descriptionDiv);
+
+    //  End of the Description Div creation
+
+
+    //  Start of the Priority Div creation
+    
+    const priorityLabel = labelMaker("Priority", "toDopriority");
+    const prioritySelect = mySelectMaker('form-control', ["Low", "Medium", "High"]);
+    const priorityDiv = classDivMaker('form-group');
+    priorityDiv.appendChild(priorityLabel);
+    priorityDiv.appendChild(prioritySelect)
+    form.appendChild(priorityDiv);
+
+    //  End of the Select Div creation
+
+    const button = document.createElement('button');
+    button.className = "btn btn-primary";
+    button.innerHTML = "Create To Do";
+    
+    form.appendChild(button);
+
+    parentNode.appendChild(form);
+
+    return parentNode;
+  }
+
+
+  export default formLoader;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello To Do');
+import './scss/index.scss';
+ import formLoader from './form';
+
+
+  let content = document.querySelector('#content')
+  content.className = "container";
+  let entryForm = (formLoader(document.createElement('div')));
+  content.appendChild(entryForm);
+
+  
+ 
